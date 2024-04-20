0afdffdcf6aed83c14a160c8878585edbb34c17c
Vinayak <hosamanivinayak5@gmail.com>
2024-04-20 16:22:18 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,7 @@ const app=express();
 const mainrouter=require("./routes/index");
 const bodyParser = require("body-parser");
 
-app.use(cors({
-    origin:["https://paytm-basic-frontend-tau.vercel.app/"],
-    methods:["POST","GET","PUT"],
-    credentials:true
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1",mainrouter);
 const port=3000;
