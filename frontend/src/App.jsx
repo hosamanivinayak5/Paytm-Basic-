c1b9eecfba704925bdd13e064881b7a637ab5b0e
Vinayak <hosamanivinayak5@gmail.com>
2024-04-20 15:57:55 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/signin" element={<Signin/>}></Route>
+      <Route path="/signup" element={<Signup/>}>SIGNUP</Route>
+      <Route path="/signin" element={<Signin/>}>SIGIN</Route>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
       <Route path="/send" element={<Send/>}></Route>
     </Routes>
