34ffc90371aea3fa9323afdfa6e5e3b9d07430d8
Vinayak <hosamanivinayak5@gmail.com>
2024-04-20 17:32:53 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,13 @@ app.use(cors({
     credentials:true
 }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
+      next();
+    });
 app.use("/api/v1",mainrouter);
 const port=3000;
 app.listen(port,()=>{
