8b0218cff8ccc6e2dd78daa0e017cc2555620c54
Vinayak <hosamanivinayak5@gmail.com>
2024-04-20 16:46:37 +0530

    CHANGED

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ const app=express();
 const mainrouter=require("./routes/index");
 const bodyParser = require("body-parser");
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://paytm-basic-frontend-tau.vercel.app"],
+        methods:["POST","GET","PUT"],
+        credentials:true
+    }
+));
 app.use(bodyParser.json());
 app.use("https://paytm-basic-pi.vercel.app/api/v1/",mainrouter);
