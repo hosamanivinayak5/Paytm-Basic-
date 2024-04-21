23f5318b619f94e342ce1eb2192d47c93cfa705c
Vinayak <hosamanivinayak5@gmail.com>
2024-04-21 12:58:21 +0530

    changes in sigin

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { BottomWarning } from "../components/Bottomwarning";
 import { ButtonComponent } from "../components/Button";
 import { Heading } from "../components/heading";
@@ -5,7 +6,8 @@ import { Inputbox } from "../components/inputbox";
 import { Subheading } from "../components/subheading";
 
 export function Signin()
-{   return<div className="bg-slate-300 flex justify-center   h-screen	">
+{   const navigate=useNavigate();
+    return<div className="bg-slate-300 flex justify-center   h-screen	">
     <div className="flex flex-col justify-center">
         <div className="bg-white p-1 rounded-lg h-max text-center px-2 w-80">
  <Heading label={"Sign in"}></Heading>
@@ -13,7 +15,16 @@ export function Signin()
 <Inputbox label={"Username"} placeholder={"sadf@gmail.com"}/>
 <Inputbox label={"Password"} placeholder={"dsfasfd"}/>
 <div className="pt-4">
-    <ButtonComponent label={"Sign in"} onClick={"/dashboard"}/>
+    <ButtonComponent label={"Sign in"} onClick={async()=>{
+        const response=await  axios.post("https://paytm-basic-pi.vercel.app/api/v1/user/signup",{
+            username,
+            firstname:Firstname,
+            lastname:lastname,
+            password:PASSWORD
+        })
+    localStorage.setItem("token",response.data.token)
+    navigate("/dashboard")
+    }}/>
 </div>
 <BottomWarning label={"Don't have an account " } buttontext={"Sign up"} to={"/signup"}/>
 </div>
