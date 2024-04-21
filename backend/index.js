de0667d020071da7b861e33526233216fa330311
Vinayak <hosamanivinayak5@gmail.com>
2024-04-21 13:09:00 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,12 @@ const mainrouter=require("./routes/index");
 const bodyParser = require("body-parser");
 
 app.use(cors({
-    
+    origin:["https://paytm-basic-pi.vercel.app"],
+    methods:["POST","PUT","GET","DELETE"],
+    credentials:true
 }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
-      next();
-    });
+
 app.use("/api/v1",mainrouter);
 const port=3000;
 app.listen(port,()=>{
