4ccc030c43d73764f24b40ba820ea17221c6c3f9
Vinayak <hosamanivinayak5@gmail.com>
2024-04-21 12:48:54 +0530

    git commit

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ const mainrouter=require("./routes/index");
 const bodyParser = require("body-parser");
 
 app.use(cors({
-    origin:["https://paytm-basic-frontend-tau.vercel.app/"],
-    methods:["POST","GET","PUT"],
-    credentials:true
+    
 }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
