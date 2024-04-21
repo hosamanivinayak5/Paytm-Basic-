0b381eebbdb70f6bee874f93c77c4fee43126b37
Vinayak <hosamanivinayak5@gmail.com>
2024-04-21 13:48:15 +0530

    testign

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes,Route} from "react-router-dom"
+import {BrowserRouter, Routes,Route, Link} from "react-router-dom"
 import {Signup} from "./pages/Signup"
 import { Signin } from "./pages/Signin"
 import { Dashboard } from "./pages/Dashboard"
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes >
-      <Route path="/signup" element={<Signup/>}>SIGNUP</Route>
+      <Route path="/signup" element={<Signup/>}><Link>SIGNUP</Link></Route>
       <Route path="/signin" element={<Signin/>}>SIGIN</Route>
       <Route path="/dashboard" element={<Dashboard/>}></Route>
       <Route path="/send" element={<Send/>}></Route>
